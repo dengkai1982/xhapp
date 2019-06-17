f47d41faaf0cb5f7e94e7ecf4c61e393eb4114c9
dengkai <kingdengkai@163.com>
2019-06-17 08:53:15 +0800

    增加到期时间

--- src/main/java/kaiyi/app/xhapp/entity/jobs/Certificate.java
+++ src/main/java/kaiyi/app/xhapp/entity/jobs/Certificate.java
@@ -3,10 +3,12 @@ package kaiyi.app.xhapp.entity.jobs;
 import kaiyi.app.xhapp.entity.AbstractEntity;
 import kaiyi.app.xhapp.entity.access.Account;
 import kaiyi.puer.commons.data.ICurrency;
+import kaiyi.puer.commons.data.IDate;
 import kaiyi.puer.commons.validate.NotEmpty;
 import kaiyi.puer.h5ui.annotations.*;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Entity(name=Certificate.TABLE_NAME)
 @PageEntity(showName = "个人证书",entityName = "certificate",serviceName = "certificateService")
@@ -16,18 +18,26 @@ public class Certificate extends AbstractEntity {
     @NotEmpty(hint = "证书名称必须填写")
     @PageField(label = "证书名称")
     private String name;
-    @PageField(label = "证书图片",type = FieldType.DOCUMENT)
-    @FieldDocument
-    private String photo;
     @ICurrency
     @PageField(label = "期望价值",type = FieldType.NUMBER)
     @FieldNumber(type = FieldNumber.TYPE.INT)
     private int expect;
+    @IDate(pattern = "yyyy-MM-dd")
+    @PageField(label = "到期时间",type = FieldType.DATE)
+    private Date expire;
+    @PageField(label = "证书图片",type = FieldType.DOCUMENT,tableLength = 3,showTable = false,showQuery = false,
+    showSearch = false)
+    @FieldDocument
+    private String photo;
     //所有者
-    @PageField(label = "所有人",type = FieldType.REFERENCE,showForm = false,
+    @PageField(label = "所有人",type = FieldType.REFERENCE,showForm = false,tableLength = 3,
             showTable = false,showQuery = false,showDetail = false,showSearch = false)
     @FieldReference(fieldName = "phone")
     private Account owner;
+    @PageField(label = "期望价值",type = FieldType.AREATEXT,showSearch = false,showQuery = false,
+    showTable = false)
+    @FieldArea(row = 4)
+    private String remark;
 
     public String getName() {
         return name;
@@ -60,5 +70,21 @@ public class Certificate extends AbstractEntity {
 
     public void setOwner(Account owner) {
         this.owner = owner;
+    }
+    @Temporal(TemporalType.TIMESTAMP)
+    public Date getExpire() {
+        return expire;
+    }
+
+    public void setExpire(Date expire) {
+        this.expire = expire;
+    }
+    @Lob
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark;
     }
 }
